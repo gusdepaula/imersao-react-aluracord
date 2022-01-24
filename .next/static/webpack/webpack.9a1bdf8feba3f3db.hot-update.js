f654568216893507a3978c66ec4628d46a0c9c5b
Gustavo de Paula Santos <33902609+gusdepaula@users.noreply.github.com>
2022-01-24 13:03:57 -0300

    Install SkynexUI

--- .next/static/webpack/webpack.9a1bdf8feba3f3db.hot-update.js
+++ .next/static/webpack/webpack.9a1bdf8feba3f3db.hot-update.js
@@ -35,7 +35,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "e28f640a36583edc"; }
+/******/ 	__webpack_require__.h = function() { return "490c209a2d30ce2b"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/harmony module decorator */
